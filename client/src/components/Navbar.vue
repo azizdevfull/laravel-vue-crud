deac2d8bb93cd4b462cc135ac4955df580772d60
Azizbek Isroilov <aziz16110904@gmail.com>
2023-09-28 14:18:34 +0500

    completed navbar setup

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div
+        class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom"
+    >
+        <RouterLink
+            :to="{ name: 'home' }"
+            class="d-flex align-items-center link-body-emphasis text-decoration-none"
+        >
+            <VueIcon />
+        </RouterLink>
+
+        <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+                <RouterLink
+                    :to="{ name: 'home' }"
+                    class="me-3 py-2 link-body-emphasis text-decoration-none"
+                    >Home</RouterLink
+                >
+                <RouterLink
+                    :to="{ name: 'about' }"
+                    class="me-3 py-2 link-body-emphasis text-decoration-none"
+                    >About</RouterLink
+                >
+        </nav>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,11 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import uiComponents from "./ui-components";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
+uiComponents.map((component) => app.component(component.name, component));
 
-const app = createApp(App)
+app.use(router);
 
-app.use(router)
-
-app.mount('#app')
+app.mount("#app");
